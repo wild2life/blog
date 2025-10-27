714c99e8407bba457243e6dfccda53a363369cd7
wildlife <chimchim333@163.com>
2025-10-27 14:50:40 +0800

    build: 优化 tailwindcss 配置

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -83,5 +83,12 @@ export default defineConfig({
 
   vite: {
     plugins: [MarkdownPreview()],
+     css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler',
+        },
+      },
+    },
   },
 })
